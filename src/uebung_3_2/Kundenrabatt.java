7c8ea95e4c1243447d6b5edf3f332646c138cb5a
15497 <15497@B004-09.nordakademie.intern>
2017-11-03 16:33:51 +0100

    Kundenrabatt programmiert

--- src/uebung_3_2/Kundenrabatt.java
+++ src/uebung_3_2/Kundenrabatt.java
@@ -5,28 +5,29 @@ public class Kundenrabatt {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		double bestellwert = 2;
-		double bisherigesKundenBestellvolumen = 1;
+		double bestellwert = 5001;
+		double bisherigesKundenBestellvolumen = 10000;
 		boolean istPremiumKunde = false;
 		double rabatt;
 		double rabatt1;
 		double rabatt2;
+		double maxrabatt1 = 0.2;
+		double maxrabatt2 = 0.15;
 
-		System.out.println("Bestellwert ohne Rabatt = " + bestellwert);
-
-		if (bestellwert > 100) {
-			rabatt = 0.01;
+		if (bestellwert <= 100) {
+			rabatt = 0;
 
 		} else {
-			if (bestellwert > 100) {
-				rabatt = 0.01;
+			
+			if (bestellwert < 500) {
+			rabatt = 0.01;
 
-			} else {
-				if (bestellwert > 500) {
+			} else {			
+				if (bestellwert < 2000) {
 					rabatt = 0.02;
 
 				} else {
-					if (bestellwert > 2000) {
+					if (bestellwert < 5000) {
 						rabatt = 0.03;
 
 					} else {
@@ -35,7 +36,9 @@ public class Kundenrabatt {
 					}
 				}
 			}
+		
 		}
+		
 		if (bisherigesKundenBestellvolumen > 10000) {
 			rabatt1 = 0.075;
 		} else {
@@ -46,7 +49,32 @@ public class Kundenrabatt {
 
 			}
 		}
-
+		if (bestellwert/2>bisherigesKundenBestellvolumen){
+			rabatt2 = 0.03;
+		} else {
+			rabatt2 = 0;
+		}
+		
+		System.out.println("Bestellwert: "+ bestellwert);
+		if (istPremiumKunde){
+			if (rabatt*2+rabatt1+rabatt2>0.2) {
+				System.out.println("Rabatt: "+ maxrabatt1);
+				System.out.println("rabattierter Bestellwert: "+ ((1-maxrabatt1)*bestellwert));
+			}else {
+				System.out.println("Rabatt: "+ (rabatt*2+rabatt1+rabatt2));
+				System.out.println("rabattierter Bestellwert: "+ ((1-(rabatt*2+rabatt1+rabatt2))*bestellwert));
+			}
+				
+			
+		}else {
+			if (rabatt+rabatt1+rabatt2>0.15) {
+				System.out.println("Rabatt: "+ maxrabatt2);
+				System.out.println("rabattierter Bestellwert: "+ ((1-maxrabatt2)*bestellwert));
+			} else {
+				System.out.println("Rabatt: "+ (rabatt+rabatt1+rabatt2));
+				System.out.println("rabattierter Bestellwert: "+ ((1-(rabatt+rabatt1+rabatt2))*bestellwert));
+			}
+		
 	}
 
-}
+	}}
